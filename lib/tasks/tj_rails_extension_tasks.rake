233ff6e9d1077f7f7a3b3a8d08ccfce14fff0936
Tony Jian <tonytonyjan@gmail.com>
2012-11-05 14:46:19 +0800

    update

--- lib/tasks/tj_rails_extension_tasks.rake
+++ lib/tasks/tj_rails_extension_tasks.rake
@@ -2,6 +2,14 @@ namespace :tj do
   desc "Apply initial template."
   task :install do
     location = File::join(File.dirname(__FILE__), "..", "templates", "init.rb")
-    system("rake rails:template LOCATION=" + location)
+    system "rake rails:template LOCATION=" + location
+    system "bundle install"
+    system "rake tj:bootstrap"
+  end
+
+  desc "Install Bootstrap"
+  task :bootstrap do
+    location = File::join(File.dirname(__FILE__), "..", "templates", "bootstrap.rb")
+    system "rake rails:template LOCATION=" + location
   end
 end
--- lib/templates/init.rb
+++ lib/templates/init.rb
@@ -23,28 +23,12 @@ if yes?("Install gems?")
   gem "rdiscount"
   ## i18n
   gem "rails-i18n"
-  ## Dev
-  gem_group :development do
-    gem "faker"
-    gem "rubyzip"
-  end
-  rake "gems:install"
 end
 
-if yes?("Install Twitter Boostrap to 'vendor/assets/' ?")
-  require 'zip/zip'
-  bootstrap_zip = open("http://twitter.github.com/bootstrap/assets/bootstrap.zip")
-  Zip::ZipFile.open(bootstrap_zip) do |zipfile|
-    zipfile.each{|entry|
-      base_name = File::basename(entry.name)
-      case entry.to_s
-      when /bootstrap\/js\/.+/
-        vendor("assets/javascripts/#{base_name}", entry.get_input_stream().read())
-      when /bootstrap\/css\/.+/
-        vendor("assets/stylesheets/#{base_name}", entry.get_input_stream().read().gsub(/".*\/(.*\.png)"/, '"\1"'))
-      when /bootstrap\/img\/.+/
-        vendor("assets/images/#{base_name}", entry.get_input_stream().read())
-      end
-    }
-  end
+## Dev
+gem_group :development do
+  gem "faker"
+  gem "rubyzip"
 end
+
+rake "db:migrate"
