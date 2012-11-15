9a5866a9f43068ca1ae8e9b1bdcbeb9304f1f936
Tony Jian <tonytonyjan@gmail.com>
2012-11-15 16:50:50 +0800

    update

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,28 +1,30 @@
-module WillPaginate
-  module ActionView
-    def will_paginate(collection = nil, options = {})
-      options[:renderer] ||= BootstrapLinkRenderer
-      super.try :html_safe
-    end
-
-    class BootstrapLinkRenderer < LinkRenderer
-      protected
-      
-      def html_container(html)
-        tag :div, tag(:ul, html), container_attributes
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActionView
+      def will_paginate(collection = nil, options = {})
+        options[:renderer] ||= BootstrapLinkRenderer
+        super.try :html_safe
       end
 
-      def page_number(page)
-        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
-      end
+      class BootstrapLinkRenderer < LinkRenderer
+        protected
+        
+        def html_container(html)
+          tag :div, tag(:ul, html), container_attributes
+        end
 
-      def previous_or_next_page(page, text, classname)
-        tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
-      end
+        def page_number(page)
+          tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+        end
 
-      def gap
-        tag :li, link(super, '#'), :class => 'disabled'
-      end
-    end
-  end
-end
+        def previous_or_next_page(page, text, classname)
+          tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+        end
+
+        def gap
+          tag :li, link(super, '#'), :class => 'disabled'
+        end
+      end # class BootstrapLinkRenderer < LinkRenderer
+    end # module ActionView
+  end # module WillPaginate
+end # if
