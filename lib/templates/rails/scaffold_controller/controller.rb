b56c023e3c63d112434e22ccd21760c948a9b451
Tony Jian <tonytonyjan@gmail.com>
2012-11-21 01:55:15 +0800

    fix controller template

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -4,15 +4,6 @@ require_dependency "<%= namespaced_file_path %>/application_controller"
 <% end -%>
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
-  before_filter :resource!
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  default_resource_actions
 end
 <% end -%>
